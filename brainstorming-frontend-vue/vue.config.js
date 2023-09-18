c9d6926a3935f5be0245725ff06ff91fbd2bde3d
lovelyweiwei <342048733@qq.com>
2023-09-18 14:58:03 +0800

    界面优化，轮询

--- brainstorming-frontend-vue/vue.config.js
+++ brainstorming-frontend-vue/vue.config.js
@@ -1,8 +1,9 @@
-const { defineConfig } = require("@vue/cli-service");
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   chainWebpack(config) {
     config.plugin("monaco").use(new MonacoWebpackPlugin({}));
-  },
+  }
 });
